e86e17b7f1c77afea662a09f40e01eda4989648e
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-05-25 20:59:18 +0400

    Add contact form to footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,30 @@
+<section>
+	<header class="major">
+		<h2>Contact_</h2>
+	</header>
+	<form method="post" action="#">
+		<div class="row uniform">
+			<div class="6u 12u$(xsmall)">
+				<input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+			</div>
+			<div class="6u$ 12u$(xsmall)">
+				<input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
+			</div>
+			<!-- Break -->
+			<div class="12u$">
+				<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+			</div>
+			<!-- Break -->
+			<div class="12u$">
+				<ul class="actions">
+					<li><input type="submit" value="Send Message" class="special" /></li>
+				</ul>
+			</div>
+		</div>
+	</form>
+	</ul>
+</section>
+
 <footer id="footer">
 	<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
 </footer>
