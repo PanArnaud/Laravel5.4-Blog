6588af4300c9fa08c5e479483d64de738be8e452
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-05-25 20:29:36 +0400

    Comment search section

--- resources/views/partials/_menu.blade.php
+++ resources/views/partials/_menu.blade.php
@@ -1,9 +1,9 @@
 <!-- Search -->
-<section id="search" class="alt">
+<!-- <section id="search" class="alt">
 	<form method="post" action="#">
 		<input type="text" name="query" id="query" placeholder="Search" />
 	</form>
-</section>
+</section> -->
 
 <!-- Menu -->
 <nav id="menu">
@@ -14,26 +14,8 @@
 		<li><a href="index.html">Homepage</a></li>
 		<li><a href="generic.html">Generic</a></li>
 		<li><a href="elements.html">Elements</a></li>
-		<li>
-			<span class="opener">Submenu</span>
-			<ul>
-				<li><a href="#">Lorem Dolor</a></li>
-				<li><a href="#">Ipsum Adipiscing</a></li>
-				<li><a href="#">Tempus Magna</a></li>
-				<li><a href="#">Feugiat Veroeros</a></li>
-			</ul>
-		</li>
 		<li><a href="#">Etiam Dolore</a></li>
 		<li><a href="#">Adipiscing</a></li>
-		<li>
-			<span class="opener">Another Submenu</span>
-			<ul>
-				<li><a href="#">Lorem Dolor</a></li>
-				<li><a href="#">Ipsum Adipiscing</a></li>
-				<li><a href="#">Tempus Magna</a></li>
-				<li><a href="#">Feugiat Veroeros</a></li>
-			</ul>
-		</li>
 		<li><a href="#">Maximus Erat</a></li>
 		<li><a href="#">Sapien Mauris</a></li>
 		<li><a href="#">Amet Lacinia</a></li>
