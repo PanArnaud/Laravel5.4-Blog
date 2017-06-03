1cd1afe320fc7f7d6f00227402adb22dd62ed488
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-06-03 20:23:05 +0400

    Add github link

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -3,6 +3,7 @@
 		<strong>@yield('title')</strong>
 	</div>
 	<ul class="icons">
-		<li><a href="linkedin.com/in/arnpan" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+		<li><a href="https://github.com/PanArnaud" class="icon fa-github"><span class="label">Github</span></a></li>
+		<li><a href="https://www.linkedin.com/in/arnpan" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
 	</ul>
 </header>
