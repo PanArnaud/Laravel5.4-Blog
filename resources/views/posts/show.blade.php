a522816ed8d317e33f5d5a3243fb27ce23703680
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-06-05 22:20:34 +0400

    Get datas from database

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,11 @@
 @extends('main')
 
-@section('title', 'Administration | Titre de l\'article')
+@section('title', 'Administration | '.$post->title)
 
 @section('content')
-  
+  <header class="main">
+    <h1><h1>{{ $post->title }}</h1></h1>
+  </header>
+
+  <p>{{ $post->body }}</p>
 @endsection
