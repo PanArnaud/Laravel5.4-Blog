71b6413fdff46b6b7bee16d66339c0d23d992145
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-06-06 18:51:16 +0400

    Show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,19 @@
 @section('title', 'Administration | '.$post->title)
 
 @section('content')
+  <ul class="icons">
+    <li>
+      <strong><i href="#" class="icon fa-{{ $post->online == 1 ? 'check' : 'times' }}"></i></strong>
+       {{ $post->online == 1 ? 'En ligne' : 'Hors ligne' }}
+    </li>
+    <li><strong>Création:</strong> <i>{{ $post->created_at }}</i></li>
+    <li><strong>Modification:</strong> <i>{{ $post->updated_at }}</i></li>
+    <li><a href="#" class="button special icon fa-search">Icon</a></li>
+    <li><a href="#" class="button icon fa-download">Icon</a></li>
+  </ul>
+
+  <hr>
+
   <header class="main">
     <h1><h1>{{ $post->title }}</h1></h1>
   </header>
