7fd38ede409049ed2af0bb4be6806d4d7f8e23f3
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-06-07 22:40:39 +0400

    Index page

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,32 @@
+@extends('main')
+
+@section('title', 'Administration | Articles')
+
+@section('content')
+  <a href="{{ route('posts.create') }}" class ="button special icon fa-pencil">Nouvel article</a>
+  <hr>
+  <div class="table-wrapper">
+    <table>
+      <thead>
+        <th width="5%">#</th>
+        <th width="10%">Titre</th>
+        <th width="55%">Contenu</th>
+        <th width="10%">Crée le</th>
+        <th width="20%"></th>
+      </thead>
+      <tbody>
+        @foreach($posts as $post)
+          <tr>
+            <td>{{ $post->id }}</td>
+            <td>{{ $post->title }}</td>
+            <td>{{ substr(strip_tags($post->body), 0, 100) }}{{ strlen(strip_tags($post->body)) > 100 ? "..." : "" }}</td>
+            <td>{{ $post->getCreatedAtAttribute($post->created_at) }}</td>
+            <td>
+              <a href="{{ route('posts.show', $post->id) }}" class ="button special small icon fa-file-text">Voir</a> <a href="{{ route('posts.edit', $post->id) }}" class ="button special small icon fa-pencil">Modifier</a>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,8 +8,8 @@
       <strong><i href="#" class="icon fa-{{ $post->online == 1 ? 'check' : 'times' }}"></i></strong>
        {{ $post->online == 1 ? 'En ligne' : 'Hors ligne' }}
     </li>
-    <li><strong>Création:</strong> <i> {{$post->getCreatedAtAttribute($post->created_at) }}</i></li>
-    <li><strong>Modification:</strong> <i> {{$post->getUpdatedAtAttribute($post->created_at) }}</i></li>
+    <li><strong>Création:</strong> <i> {{ $post->getCreatedAtAttribute($post->created_at) }}</i></li>
+    <li><strong>Modification:</strong> <i> {{ $post->getUpdatedAtAttribute($post->created_at) }}</i></li>
     <li>
       {!! Html::linkRoute('posts.edit', 'Editer', array($post->id), array('class' => 'button special small icon fa-pencil')) !!}
     </li>
