a8b7e03075ccb449f8d65e27d02cda78733c71c8
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-06-30 00:45:56 +0400

    display recents post on the index page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     function getIndex() {
-      return view('pages.index');
+      $posts = Post::orderBy('id', 'DESC')->limit(6)->get();
+      return view('pages.index')->with('posts', $posts);
     }
 }
