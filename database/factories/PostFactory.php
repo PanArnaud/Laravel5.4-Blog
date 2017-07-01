acf7ec44d5d215fe8f9b63fde94f675b4903c50a
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-07-01 15:29:22 +0400

    Add slug column, add slug to factory, and add doctrine/dbal to composer.json

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,9 +1,10 @@
 <?php
 $factory->define(App\Post::class, function (Faker\Generator $faker) {
     $faker = Faker\Factory::create('fr_FR');
-
+    $title = $faker->sentence($nbWords = 6, $variableNbWords = true);
     return [
-        'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'title' => $title,
+        'slug' => str_slug($title),
         'body' => $faker->realText($maxNbChars = 200, $indexSize = 2),
         'online' => $faker->boolean(75)
     ];
